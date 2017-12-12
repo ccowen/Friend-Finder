8aabd8892ebb1a482b4b92e529ce652460c8b3b1
ccowen <caitlincowen2016@u.northwestern.edu>
2017-12-12 11:15:13 -0600

    some functionality

---
+++ app/data/friends.js
@@ -0,0 +1,208 @@
+[{
+	"name": "Ahmed",
+	"photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+	"scores": [
+		"5",
+		"1",
+		"4",
+		"4",
+		"5",
+		"1",
+		"2",
+		"5",
+		"4",
+		"1"
+	]
+},
+{
+	"name": "Jacob Deming",
+	"photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+	"scores": [
+		"4",
+		"2",
+		"5",
+		"1",
+		"3",
+		"2",
+		"2",
+		"1",
+		"3",
+		"2"
+	]
+},
+{
+	"name": "Jeremiah Scanlon",
+	"photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+	"scores": [
+		"5",
+		"2",
+		"2",
+		"2",
+		"4",
+		"1",
+		"3",
+		"2",
+		"5",
+		"5"
+	]
+},
+{
+	"name": "Louis T. Delia",
+	"photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+	"scores": [
+		"3",
+		"3",
+		"4",
+		"2",
+		"2",
+		"1",
+		"3",
+		"2",
+		"2",
+		"3"
+	]
+},
+{
+	"name": "Lou Ritter",
+	"photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+	"scores": [
+		"4",
+		"3",
+		"4",
+		"1",
+		"5",
+		"2",
+		"5",
+		"3",
+		"1",
+		"4"
+	]
+},
+{
+	"name": "Jordan Biason",
+	"photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+	"scores": [
+		"4",
+		"4",
+		"2",
+		"3",
+		"2",
+		"2",
+		"3",
+		"2",
+		"4",
+		"5"
+	]
+},
+{
+	"name": "jen",
+	"photo": "https://static.pexels.com/photos/248797/pexels-photo-248797.jpeg",
+	"scores": [
+		"5",
+		"4",
+		"4",
+		"3",
+		"4",
+		"2",
+		"3",
+		"3",
+		"2",
+		"3"
+	]
+},
+{
+	"name": "josh",
+	"photo": "none",
+	"scores": [
+		"2",
+		"2",
+		"3",
+		"3",
+		"3",
+		"4",
+		"3",
+		"3",
+		"2",
+		"1"
+	]
+},
+{
+	"name": "kim",
+	"photo": "none",
+	"scores": [
+		"2",
+		"1",
+		"3",
+		"3",
+		"3",
+		"4",
+		"3",
+		"3",
+		"3",
+		"1"
+	]
+},
+{
+	"name": "phil",
+	"photo": "l",
+	"scores": [
+		"1",
+		"2",
+		"3",
+		"4",
+		"4",
+		"5",
+		"2",
+		"4",
+		"4",
+		"5"
+	]
+},
+{
+	"name": "ron",
+	"photo": "https://upload.wikimedia.org/wikipedia/commons/c/c7/Ron_Perlman_2016.jpg",
+	"scores": [
+		"3",
+		"1",
+		"3",
+		"2",
+		"2",
+		"4",
+		"3",
+		"5",
+		"3",
+		"1"
+	]
+},
+{
+	"name": "Dimitri Kontos",
+	"photo": "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/13336120_10206724944817912_8545125576821402897_n.jpg?oh=d9a9addf31d8b34b1af893377817a94f&oe=5ABE2305",
+	"scores": [
+		"2",
+		"5",
+		"3",
+		"3",
+		"2",
+		"1",
+		"5",
+		"3",
+		"2",
+		"5"
+	]
+},
+{
+	"name": "Joe",
+	"photo": "https://az616578.vo.msecnd.net/files/2016/05/08/635982629770669727-503228210_B0Wh1mVIAAAMu0P.jpg",
+	"scores": [
+		"1",
+		"2",
+		"3",
+		"4",
+		"5",
+		"1",
+		"2",
+		"3",
+		"4",
+		"5"
+	]
+}]
---
+++ server.js
@@ -0,0 +1,68 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var fs = require("fs");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/survey.html"));
+});
+
+// Get all friends
+app.get("/all", function(req, res) {
+  // get friends from its file
+  res.sendFile(path.join(__dirname, "app/data/friends.js"));
+});
+
+// Search for Specific Character (or all characters) - provides JSON
+// app.get("/api/:characters?", function(req, res) {
+//   var chosen = req.params.characters;
+
+//   if (chosen) {
+//     console.log(chosen);
+
+//     for (var i = 0; i < characters.length; i++) {
+//       if (chosen === characters[i].routeName) {
+//         return res.json(characters[i]);
+//       }
+//     }
+//     return res.json(false);
+//   }
+//   return res.json(characters);
+// });
+
+// Create New Characters - takes in JSON input
+app.post("/api/new", function(req, res) {
+
+  var newForm = req.body;
+
+  console.log(newForm);
+
+  //characters.push(newForm);
+
+  res.json(newForm);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
