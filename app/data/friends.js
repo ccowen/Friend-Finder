bc6d24d72de4744d38a0f4ad5875509e48c84588
ccowen <caitlincowen2016@u.northwestern.edu>
2017-12-28 21:51:46 -0600

    working app

--- app/data/friends.js
+++ app/data/friends.js
@@ -16,515 +16,147 @@
     ]
   },
   {
-    "name": "Cait",
-    "photo": "google.com",
+    "name": "Wonder Woman",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Gal_Gadot_cropped_lighting_corrected_2b.jpg/220px-Gal_Gadot_cropped_lighting_corrected_2b.jpg",
     "scores": [
-      "5",
       "2",
-      "4",
-      "3",
-      "5",
       "1",
+      "3",
+      "4",
+      "4",
       "2",
-      "5",
       "2",
-      "1"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
+      "3",
+      "4",
       "5"
     ]
   },
   {
-    "name": "",
-    "photo": "",
+    "name": "Stephen Colbert",
+    "photo": "https://pmcdeadline2.files.wordpress.com/2017/01/stephen-colbert-5.jpg?w=605",
     "scores": [
-      "1",
-      "1",
-      "1",
-      "1",
-      "1",
-      "1",
-      "1",
-      "1",
+      "4",
+      "3",
+      "2",
+      "3",
+      "4",
+      "4",
+      "3",
+      "2",
       "1",
       "1"
     ]
   },
   {
-    "name": "",
-    "photo": "",
+    "name": "Hans Zimmer",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Hans_Zimmer_crop.jpg/220px-Hans_Zimmer_crop.jpg",
     "scores": [
       "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
-      "4",
-      "4",
-      "4",
-      "4",
-      "4",
-      "4",
-      "4",
-      "4",
-      "4",
-      "4"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
-      "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
       "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
       "5",
       "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
       "1",
+      "1",
+      "5",
+      "5",
+      "1",
+      "1"
+    ]
+  },
+  {
+    "name": "Moana",
+    "photo": "https://pbs.twimg.com/profile_images/819955513718779905/DY4UfMVj_400x400.jpg",
+    "scores": [
       "2",
       "3",
-      "4",
-      "5",
-      "4",
+      "2",
       "3",
       "2",
       "1",
-      "2"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
       "4",
       "3",
-      "2",
-      "2",
-      "2",
-      "3",
-      "3",
-      "4",
-      "4",
-      "2"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
-      "4",
-      "4",
-      "4",
-      "4",
-      "4",
-      "4",
-      "4",
-      "4",
-      "4",
-      "4"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
-      "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5"
-    ]
-  },
-  {
-    "name": "",
-    "photo": "",
-    "scores": [
-      "4",
-      "4",
-      "4",
-      "4",
-      "3",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2"
-    ]
-  },
-  {
-    "name": "sally",
-    "photo": "",
-    "scores": [
-      "1",
-      "2",
-      "3",
-      "3",
-      "3",
-      "2",
-      "4",
-      "4",
-      "3",
-      "2"
-    ]
-  },
-  {
-    "name": "Ron",
-    "photo": "",
-    "scores": [
-      "3",
-      "3",
-      "4",
-      "4",
-      "3",
-      "2",
-      "1",
-      "2",
-      "1",
-      "2"
-    ]
-  },
-  {
-    "name": "Rahil",
-    "photo": "",
-    "scores": [
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5"
-    ]
-  },
-  {
-    "name": "test",
-    "photo": "",
-    "scores": [
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5"
-    ]
-  },
-  {
-    "name": "test2",
-    "photo": "",
-    "scores": [
-      "3",
-      "3",
-      "3",
-      "2",
-      "1",
-      "5",
-      "3",
-      "4",
       "2",
       "1"
     ]
   },
   {
-    "name": "test3",
-    "photo": "",
+    "name": "Deadpool",
+    "photo": "https://s3.amazonaws.com/ffe-ugc/intlportal2/dev-temp/en-US/__59a48ed7599d0.jpg",
     "scores": [
+      "1",
+      "2",
+      "3",
+      "4",
       "5",
       "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
+      "4",
+      "3",
+      "2",
+      "1"
+    ]
+  },
+  {
+    "name": "Dory",
+    "photo": "https://am23.akamaized.net/tms/cnt/uploads/2013/08/FindingDory2.jpg",
+    "scores": [
+      "1",
+      "1",
+      "2",
+      "2",
+      "3",
+      "3",
+      "4",
+      "4",
       "5",
       "5"
     ]
   },
   {
-    "name": "test4",
-    "photo": "",
+    "name": "Dr. Phil",
+    "photo": "https://img.drphil.com/wp-content/uploads/2016/12/Dr_Phil_TOP_RightSidebar2.jpg",
     "scores": [
+      "2",
       "3",
       "4",
       "3",
+      "2",
+      "3",
+      "4",
+      "5",
+      "4",
+      "3"
+    ]
+  },
+  {
+    "name": "Kumail Nanjiani",
+    "photo": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=&url=https%3A%2F%2Fwww.grinnell.edu%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fgallery_slideshow%2Fpublic%2FKumail-Nanjiani-2016.jpg%3Fitok%3DVUG-aDTr&psig=AOvVaw0QoXttDftHHxHGnrLXU4U3&ust=1514602311673353",
+    "scores": [
       "4",
       "4",
+      "5",
       "3",
       "2",
       "2",
       "1",
-      "3"
+      "3",
+      "4",
+      "2"
+    ]
+  },
+  {
+    "name": "Twitch",
+    "photo": "https://cdn1.thr.com/sites/default/files/imagecache/portrait_300x450/2013/08/stephen_twitch_boss_a_p.jpg",
+    "scores": [
+      "2",
+      "3",
+      "4",
+      "3",
+      "4",
+      "2",
+      "5",
+      "4",
+      "2",
+      "5"
     ]
   }
 ]
--- server.js
+++ server.js
@@ -40,23 +40,18 @@ function subtractScores(input1, input2) {
 var lowestScore;
 var lowestScoreArrayNumber;
 
+// function to find the lowest score in comparision to new data
 function findLowestScore(newData, allData) {
-	console.log(allData, "this is all data in the lowestscore function");
-	console.log(newData);
-
-	console.log("subtractScores in lowestscore function" + subtractScores(newData, allData[0]));
 
 	// find the first difference, for now this is the lowest
 	var firstScore = subtractScores(newData, allData[0]);
-
-	console.log("FIRSTSCORE" + firstScore);
 
 	// set the first score as the lowest score variable
 	var lowestScore = firstScore;
 	var lowestScoreArrayNumber = 0;
 
 	// compare all difference, see if there is a lower score
-	for (var i = 1; i < allData.length; i++) {
+	for (var i = 1; i < allData.length - 1; i++) {
 		var compareScore = subtractScores(newData, allData[i]);
 
 		if (compareScore < lowestScore) {
@@ -69,7 +64,8 @@ function findLowestScore(newData, allData) {
 
 		// ---- else if the scores are equal, randomize who will be matched, will not always move to the most recent matching result
 		else if (compareScore == lowestScore) {
-			if ((getRandomInt(1, 5)) >= 3) {
+			var randomInt = getRandomInt(1, 100);
+			if (randomInt >= 27) {
 				lowestScore = compareScore;
 			 	lowestScoreArrayNumber = [i];
 
@@ -117,29 +113,25 @@ app.post("/api/new", function(req, res) {
   // declare variable for the closet match
   var lowestScoreArrayNumber;
  
+  // read the file with the data
   jsonfile.readFile(file, function(err, obj) {
-	  console.log("FIRST CONSOLE LOG OBJ" + obj);
-
-	  // currentData = obj;
 
   	  obj.push(newForm);
 
+  	  // write the new data to the form
   	  jsonfile.writeFile(file, obj, {spaces: 2, EOL: '\r\n'}, function(err) {
 	  	console.log(err);
 
+	  	// find the lowest score in comparision to the newForm 
 	    lowestScoreArrayNumber = findLowestScore(newForm, obj);
 
-	    console.log(obj[lowestScoreArrayNumber]);
-	    console.log(obj[lowestScoreArrayNumber].name);
-	    console.log(obj[lowestScoreArrayNumber].photo);
-
-	    var resultDataForModal = "<p class='flow-text'>Score: <span id='modalLowestScore'></span>" +
+	    // content to put into modal
+	    var resultDataForModal = "<p class='flow-text'>" +
+			  	"<img id='modalLowestScorePicture' src='" + obj[lowestScoreArrayNumber].photo + "'>" +
 			  	"<br>Name: <span id='modalLowestScoreName'>" + obj[lowestScoreArrayNumber].name + "</span>" +
-			  	"<br>Picture: <img id='modalLowestScorePicture'src='" + obj[lowestScoreArrayNumber].photo + "'>" +
-			  	"<br>" +
 			  "</p>";
 
-
+		// send to front end
 	    res.send(resultDataForModal);
 
 	  });
